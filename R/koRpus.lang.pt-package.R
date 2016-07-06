6e9b477cf6742b1c36205779f8c0471d4a530c50
unDocUMeantIt <m@reaktanz.de>
2016-07-06 11:23:42 +0200

    re-formatted description text

--- R/koRpus.lang.pt-package.R
+++ R/koRpus.lang.pt-package.R
@@ -4,7 +4,7 @@
 #' Package: \tab koRpus.lang.pt\cr
 #' Type: \tab Package\cr
 #' Version: \tab 0.01-1\cr
-#' Date: \tab 2016-07-05\cr
+#' Date: \tab 2016-07-06\cr
 #' Depends: \tab R (>= 2.10.0),koRpus (>= 0.07-1), methods\cr
 #' Encoding: \tab UTF-8\cr
 #' License: \tab GPL (>= 3)\cr
@@ -12,7 +12,11 @@
 #' URL: \tab http://reaktanz.de/?c=hacking&s=koRpus\cr
 #' }
 #'
-#' Adds support for the Portuguese language to the koRpus package. Due to some restrictions on CRAN, the full package sources are only available from the project homepage. To ask for help, report bugs, suggest feature improvements, or discuss the global development of the package, please subscribe to the koRpus-dev mailing list: https://ml06.ispgateway.de/mailman/listinfo/korpus-dev_r.reaktanz.de
+#' Adds support for the Portuguese language to the koRpus package. Due to some restrictions
+#' on CRAN, the full package sources are only available from the project homepage. To ask for
+#' help, report bugs, suggest feature improvements, or discuss the global development of the
+#' package, please subscribe to the koRpus-dev mailing list:
+#' https://ml06.ispgateway.de/mailman/listinfo/korpus-dev_r.reaktanz.de
 #'
 #' @aliases koRpus.lang.pt-package
 #' @name koRpus.lang.pt-package
